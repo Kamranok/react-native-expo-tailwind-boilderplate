1478a1e91b59e4bbebfd94e69739f1dc9e674de0
Kamran Muhammad <a0016232@securustechnologies.com>
2024-07-25 14:22:10 -0500

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-red-700 text-3xl'>Home Page</Text>
+    </View>
+  )
+}
+
+export default HomePage
+// text-red-700 text-3xl
